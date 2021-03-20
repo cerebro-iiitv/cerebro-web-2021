29df0fcea0fc7109ba642e167fcd8d816c3b6ad0
Anuj Puri <anujpuri72@gmail.com>
2021-03-20 23:41:41 +0530

    new_jai_shree_ram

--- src/Components/UserDashboard/UserDashboard.jsx
+++ src/Components/UserDashboard/UserDashboard.jsx
@@ -2,54 +2,94 @@ import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import RegisteredEvent from "../UserDashboard/RegisteredEvent/RegisteredEvent";
-import "./UserDashboard.scss"
+import "./UserDashboard.scss";
 import axios from "axios";
 
 class UserDashboard extends React.Component {
-
   state = {
-    details: {}
-  }
+    details: {},
+    number: "",
+  };
 
   componentDidMount() {
-    const user = JSON.parse(localStorage.getItem('user'));
+    const user = JSON.parse(localStorage.getItem("user"));
     if (user) {
-      axios.get(`https://cerebro.pythonanywhere.com/dashboard/${user.user_id}`, {
-        headers: {
-          'Authorization': `Token ${user.access_token}`
-        }
-      }).then(res=> {
-        this.setState({ details: res })
-      })
+      axios
+        .get(`https://cerebro.pythonanywhere.com/dashboard/${user.user_id}`, {
+          headers: {
+            Authorization: `Token ${user.access_token}`,
+          },
+        })
+        .then((res) => {
+          console.log(res);
+          this.setState({
+            details: res,
+            number: res.data?.mobile_number,
+          });
+        });
     }
   }
-
-  onDeleteEvent = (id) => {
-    const user = JSON.parse(localStorage.getItem('user'));
-
-    axios.delete(`https://cerebro.pythonanywhere.com/registration/team-register/${id}/`, {
-        headers: {
-          'Authorization':`Token ${user.access_token}`
-        },
-    }).then(res => {
-        const temp = this.state.details.data?.user_team.filter(event => {
-          return event.id !== id
-        })
-        const obj = {
-          ...this.state.details
+  onNumberSubmit = () => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    console.log(user);
+    axios
+      .patch(
+        `https://cerebro.pythonanywhere.com/dashboard/${user.user_id}/`,
+        { mobile_number: this.state.number },
+        {
+          headers: {
+            Authorization: `Token ${user.access_token}`,
+          },
         }
-        obj.data.user_team = temp
-        this.setState({
-          details: obj
-        })
-      })
-  }
+      )
+      .then((res) => {
+        console.log(res);
+      });
+  };
+  onDeleteEvent = (id) => {
+    const user = JSON.parse(localStorage.getItem("user"));
 
+    axios
+      .delete(
+        `https://cerebro.pythonanywhere.com/registration/team-register/${id}`,
+        {
+          headers: {
+            Authorization: `Token ${user.access_token}`,
+          },
+        }
+      )
+      .then((res) => {
+        const temp = this.state.details.data?.user_team.filter((event) => {
+          return event.id !== id;
+        });
+        const obj = {
+          ...this.state.details,
+        };
+        obj.data.user_team = temp;
+        this.setState({
+          details: obj,
+          // number: this.state.details.data.mobile_number,
+          number: this.state.details.data?.mobile_number,
+        });
+      });
+  };
 
   render() {
-
-    const RegisteredEvents = this.state.details.data?.user_team?.length ? this.state.details.data.user_team.map((e) => { return <RegisteredEvent name={e.event_name} start={e.start_time} end={e.end_time} code={e.team_code} id={e.id} onDeleteEvent={this.onDeleteEvent} /> }) : null;
-
+    const RegisteredEvents = this.state.details.data?.user_team?.length
+      ? this.state.details.data.user_team.map((e) => {
+          return (
+            <RegisteredEvent
+              name={e.event_name}
+              start={e.start_time}
+              end={e.end_time}
+              code={e.team_code}
+              id={e.id}
+              onDeleteEvent={this.onDeleteEvent}
+            />
+          );
+        })
+      : null;
+    console.log(this.state);
     return (
       <div>
         <Header />
@@ -70,12 +110,18 @@ class UserDashboard extends React.Component {
               <span>{this.state.details.data?.first_name}</span>
             </div>
             <div>
-
-              {this.state.details.mobile_number && <div><div className="user-details__number">
-                <span>{this.state.details.mobile_number}</span>
-              </div>
-                <div className="user-details__circle"></div></div>}
-
+              <input
+                type="tel"
+                value={this.state.number}
+                className="user-details__number"
+                onChange={(e) =>
+                  this.setState({ ...this.state, number: e.target.value })
+                }>
+                {/* <span>{this.state.details.mobile_number}</span> */}
+              </input>
+              <button className="userSubmit" onClick={this.onNumberSubmit}>
+                Save
+              </button>
             </div>
           </div>
         </div>
@@ -83,7 +129,6 @@ class UserDashboard extends React.Component {
       </div>
     );
   }
-
 }
 
 export default UserDashboard;
