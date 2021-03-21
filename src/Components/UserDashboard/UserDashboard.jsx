e2a80b96eb77ec1cac94ee504309009dfc6a435a
Yash Shah <201851150@iiitvadodara.ac.in>
2021-03-21 11:04:01 +0530

    removed console logs

--- src/Components/UserDashboard/UserDashboard.jsx
+++ src/Components/UserDashboard/UserDashboard.jsx
@@ -6,129 +6,143 @@ import "./UserDashboard.scss";
 import axios from "axios";
 
 class UserDashboard extends React.Component {
-  state = {
-    details: {},
-    number: "",
-  };
+    state = {
+        details: {},
+        number: "",
+    };
 
-  componentDidMount() {
-    const user = JSON.parse(localStorage.getItem("user"));
-    if (user) {
-      axios
-        .get(`https://cerebro.pythonanywhere.com/dashboard/${user.user_id}`, {
-          headers: {
-            Authorization: `Token ${user.access_token}`,
-          },
-        })
-        .then((res) => {
-          console.log(res);
-          this.setState({
-            details: res,
-            number: res.data?.mobile_number,
-          });
-        });
+    componentDidMount() {
+        const user = JSON.parse(localStorage.getItem("user"));
+        if (user) {
+            axios
+                .get(
+                    `https://cerebro.pythonanywhere.com/dashboard/${user.user_id}`,
+                    {
+                        headers: {
+                            Authorization: `Token ${user.access_token}`,
+                        },
+                    }
+                )
+                .then((res) => {
+                    this.setState({
+                        details: res,
+                        number: res.data?.mobile_number,
+                    });
+                });
+        }
     }
-  }
-  onNumberSubmit = () => {
-    const user = JSON.parse(localStorage.getItem("user"));
-    console.log(user);
-    axios
-      .patch(
-        `https://cerebro.pythonanywhere.com/dashboard/${user.user_id}/`,
-        { mobile_number: this.state.number },
-        {
-          headers: {
-            Authorization: `Token ${user.access_token}`,
-          },
-        }
-      )
-      .then((res) => {
-        console.log(res);
-      });
-  };
-  onDeleteEvent = (id) => {
-    const user = JSON.parse(localStorage.getItem("user"));
+    onNumberSubmit = () => {
+        const user = JSON.parse(localStorage.getItem("user"));
+        axios
+            .patch(
+                `https://cerebro.pythonanywhere.com/dashboard/${user.user_id}/`,
+                { mobile_number: this.state.number },
+                {
+                    headers: {
+                        Authorization: `Token ${user.access_token}`,
+                    },
+                }
+            )
+            .then((res) => {});
+    };
+    onDeleteEvent = (id) => {
+        const user = JSON.parse(localStorage.getItem("user"));
 
-    axios
-      .delete(
-        `https://cerebro.pythonanywhere.com/registration/team-register/${id}`,
-        {
-          headers: {
-            Authorization: `Token ${user.access_token}`,
-          },
-        }
-      )
-      .then((res) => {
-        const temp = this.state.details.data?.user_team.filter((event) => {
-          return event.id !== id;
-        });
-        const obj = {
-          ...this.state.details,
-        };
-        obj.data.user_team = temp;
-        this.setState({
-          details: obj,
-          // number: this.state.details.data.mobile_number,
-          number: this.state.details.data?.mobile_number,
-        });
-      });
-  };
+        axios
+            .delete(
+                `https://cerebro.pythonanywhere.com/registration/team-register/${id}`,
+                {
+                    headers: {
+                        Authorization: `Token ${user.access_token}`,
+                    },
+                }
+            )
+            .then((res) => {
+                const temp = this.state.details.data?.user_team.filter(
+                    (event) => {
+                        return event.id !== id;
+                    }
+                );
+                const obj = {
+                    ...this.state.details,
+                };
+                obj.data.user_team = temp;
+                this.setState({
+                    details: obj,
+                    number: this.state.details.data?.mobile_number,
+                });
+            });
+    };
 
-  render() {
-    const RegisteredEvents = this.state.details.data?.user_team?.length
-      ? this.state.details.data.user_team.map((e) => {
-          return (
-            <RegisteredEvent
-              name={e.event_name}
-              start={e.start_time}
-              end={e.end_time}
-              code={e.team_code}
-              id={e.id}
-              onDeleteEvent={this.onDeleteEvent}
-            />
-          );
-        })
-      : null;
-    console.log(this.state);
-    return (
-      <div>
-        <Header />
-        <div className="user">
-          <div className="user-events">
-            <span className="user-events__header">Registered Events</span>
-            <div className="user-events__registered">{RegisteredEvents}</div>
-          </div>
-          {/* <div className="user-hr-container">
+    render() {
+        const RegisteredEvents = this.state.details.data?.user_team?.length
+            ? this.state.details.data.user_team.map((e) => {
+                  return (
+                      <RegisteredEvent
+                          name={e.event_name}
+                          start={e.start_time}
+                          end={e.end_time}
+                          code={e.team_code}
+                          id={e.id}
+                          onDeleteEvent={this.onDeleteEvent}
+                      />
+                  );
+              })
+            : null;
+        return (
+            <div>
+                <Header />
+                <div className="user">
+                    <div className="user-events">
+                        <span className="user-events__header">
+                            Registered Events
+                        </span>
+                        <div className="user-events__registered">
+                            {RegisteredEvents}
+                        </div>
+                    </div>
+                    {/* <div className="user-hr-container">
                   <hr className="user-hr"/>
                 </div> */}
 
-          <div className="user-details">
-            <div>
-              <img className="spaceship" src="media/spaceShip.svg" alt="" />
+                    <div className="user-details">
+                        <div>
+                            <img
+                                className="spaceship"
+                                src="media/spaceShip.svg"
+                                alt=""
+                            />
+                        </div>
+                        <div className="user-details__name">
+                            <span>{this.state.details.data?.first_name}</span>
+                        </div>
+                        <div>
+                            <input
+                                type="tel"
+                                value={this.state.number}
+                                className="user-details__number"
+                                onChange={(e) =>
+                                    this.setState({
+                                        ...this.state,
+                                        number: e.target.value,
+                                    })
+                                }
+                            >
+                                {/* <span>{this.state.details.mobile_number}</span> */}
+                            </input>
+                            <button
+                                className="userSubmit"
+                                onClick={this.onNumberSubmit}
+                            >
+                                Save
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <Footer />
             </div>
-            <div className="user-details__name">
-              <span>{this.state.details.data?.first_name}</span>
-            </div>
-            <div>
-              <input
-                type="tel"
-                value={this.state.number}
-                className="user-details__number"
-                onChange={(e) =>
-                  this.setState({ ...this.state, number: e.target.value })
-                }>
-                {/* <span>{this.state.details.mobile_number}</span> */}
-              </input>
-              <button className="userSubmit" onClick={this.onNumberSubmit}>
-                Save
-              </button>
-            </div>
-          </div>
-        </div>
-        <Footer />
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default UserDashboard;
