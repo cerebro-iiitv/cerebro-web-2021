088f5c45dd2e121fac1a28569eee65eb7406ca87
Tanmay Ambadkar <42721553+TanmayAmbadkar@users.noreply.github.com>
2021-03-23 08:47:53 +0530

    Update Cover.jsx

--- src/Components/Home/Cover/Cover.jsx
+++ src/Components/Home/Cover/Cover.jsx
@@ -71,6 +71,11 @@ function Cover() {
                     <Countdown date={`2021-03-27T12:00:00`} />
                     <p className="t">For the launch</p>
                 </div>
+                <p className="cover__titles__date">
+                    <span>
+                        Join the <a href="https://discord.gg/YUEeUcBtbU">discord server<a> for further communication!
+                    </span>
+                </p>
             </div>
             <div className="cover__image">
                 <img
